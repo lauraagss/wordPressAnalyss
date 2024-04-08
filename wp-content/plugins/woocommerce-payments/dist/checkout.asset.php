e1ae4f88a14b538fb585954dddb3332c413178e5
Laura Goncalves <lauragoncalves931704@gmail.com>
2024-04-08 16:23:28 +0200

    fix extension and css

--- wp-content/plugins/woocommerce-payments/dist/checkout.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-data', 'wp-dom-ready', 'wp-i18n', 'wp-polyfill'), 'version' => '3394e9ea2a396eb74712');
--- wp-content/plugins/woocommerce-payments/dist/payment-request.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'wp-dom-ready', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'b05cc8c4e974b309bf3c');
